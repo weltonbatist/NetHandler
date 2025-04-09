4ab305164b274fa07011ed97fced0f3ae151dc21
welton <weltonbatist@gmail.com>
2025-04-08 23:30:20 -0300

    Iniciando projeto de teste

---
+++ src/NetHandler.Test/DispatchrTests.cs
@@ -0,0 +1,11 @@
+namespace NetHandler.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- src/NetHandler/Dispatchr.cs
+++ src/NetHandler/Dispatchr.cs
@@ -1,9 +1,4 @@
-using System;
 using System.Collections.Concurrent;
-using System.Linq;
-using System.Reflection;
-using System.Threading;
-using System.Threading.Tasks;
 using NetHandler.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
