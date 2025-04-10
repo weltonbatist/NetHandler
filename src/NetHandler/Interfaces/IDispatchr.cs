1e785a394b3b4cc68191e2ba64e7c0ebf5c8c332
skyliatech <desenvolvimento@skyliatech.com>
2025-04-09 22:28:50 -0300

    Adicionado opção de execução do PublishAsync em paralelo através do parâmetro isParallel

--- src/NetHandler/Interfaces/IDispatchr.cs
+++ src/NetHandler/Interfaces/IDispatchr.cs
@@ -3,6 +3,6 @@ namespace NetHandler.Interfaces;
 public interface IDispatchr
 {
     Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
-    Task PublishAsync<TNotification>(TNotification notification, CancellationToken cancellationToken = default)
+    Task PublishAsync<TNotification>(TNotification notification, bool isParallel = false, CancellationToken cancellationToken = default)
         where TNotification : INotification;
 }
